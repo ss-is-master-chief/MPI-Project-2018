4dd07c21a27802a175845c9cc8275bd6b35034d8
harsh-97 <30751873+harsh-97@users.noreply.github.com>
2018-03-27 02:18:04 +0530

    Update master.ino

--- master/master.ino
+++ master/master.ino
@@ -1,10 +1,14 @@
+#include "pitches.h"
 // To include audio driver code
 
 // Setting timeout for score calculation, in seconds
 float timeout = 0.3;
 
-// Setting time between two notes, in seconds
-float noteDelay = 5;
+// Setting duration for each note, in seconds
+float noteDuration = 0.5;
+
+// Setting note value, for pitches
+float noteValue = 1/noteDuration;
 
 // Setting time to display result message, in seconds
 float resultDelay = 10;
@@ -12,74 +16,127 @@ float resultDelay = 10;
 // Setting replay wait time
 float replay = 10;
 
+// Setting speaker pin
+int speakerPin = 8;
+
 // Declaring game variables
-float score1, score2;
+long score1, score2;
 int currentNote;
 String noteSequence;
 
 void setup() {
   //  Opening channel at 9600 Baud
   Serial.begin(9600);
-
+  
   currentNote = 0;
-  noteSequence = "ABCDEABEDAC";
+  noteSequence = "ABCDEZABCDEZABCDEZ";
 
   // Setting maximum value of score1 and score 2, in case one fails to transmit
-  score1 = score2 = noteSequence.length()*timeout;
+  score1 = score2 = noteSequence.length()*2.3*noteDuration;
 }
 
 void loop() {
+  Serial.flush();
   if(noteSequence[currentNote] == '\0')
   { 
     // Sending STOP signal
-    Serial.write("STOP");
+    Serial.write('S');
     
     // Getting scores from slaves
-    score1 = Serial.parseFloat();
+    if(Serial.available())
+      score1 = Serial.parseFloat();
     // Delay to prevent collision of inputs
     delay(timeout * 1000);
-    score2 = Serial.parseFloat();
+    if(Serial.available())
+      score2 = Serial.parseFloat();
 
-    if(score1 > score2)
-      Serial.write("ONE WINS");
-    else if (score2 > score1)
-      Serial.write("TWO WINS");
+//    Serial.println(score1);
+//    Serial.println(score2);
+    
+
+    if(score1 < score2)
+      Serial.write('1');
+    else if (score2 < score1)
+      Serial.write('2');
     else
-      Serial.write("TIE");
+      Serial.write('T');
 
      delay(resultDelay*1000);
      
-     delay(replay * 1000);
+     delay(replay*1000);
      // Resetting game
      currentNote = 0;
+     score1 = score2 = noteSequence.length()*2.3*noteDuration;
   }
   else
   {
    switch(noteSequence[currentNote++])
    {
     case 'A':
-      // Play note here
-      Serial.write("A");
+      Serial.write('A');
+      sing('A', noteDuration);
       break;
     case 'B':
-      // Play note here
-      Serial.write("B");
+      Serial.write('B');
+      sing('B', noteDuration);
       break;
     case 'C':
-      // Play note here
-      Serial.write("C");
-      break;
+      Serial.write('C');
+      sing('C', noteDuration);
+      break;    
     case 'D':
-      // Play note here
-      Serial.write("D");
+      Serial.write('D');
+      sing('D', noteDuration);
       break;
     case 'E':
-      // Play note here
-      Serial.write("E");
+      Serial.write('E');
+      sing('E', noteDuration);
+      break;
+    case 'F':
+      Serial.write('F');
+      sing('F', noteDuration);
+      break;
+    case 'G':
+      Serial.write('G');
+      sing('G', noteDuration);
       break;      
    }
   }
-  
-  // Pause to allow user to press note
-  delay(noteDelay * 1000);
+}
+
+void sing(char note, float noteDuration)
+{  
+  if(note=='A'){
+    tone(speakerPin, NOTE_A4, 1000*noteDuration);
+    delay(noteDuration * 1.30 * 1000);
+  }
+  else if(note=='B'){
+    tone(speakerPin, NOTE_B4, 1000*noteDuration);
+    delay(noteDuration * 1.30 * 1000);
+  }
+   else if(note=='C'){
+    tone(speakerPin, NOTE_C4, 1000*noteDuration);
+    delay(noteDuration * 1.30 * 1000);
+  }
+   else if(note=='D'){
+    tone(speakerPin, NOTE_D4, 1000*noteDuration);
+    delay(noteDuration * 1.30 * 1000);
+  }
+   else if(note=='E'){
+    tone(speakerPin, NOTE_E4, 1000*noteDuration);
+    delay(noteDuration * 1.30 * 1000);
+  }
+  else if(note=='F'){
+    tone(speakerPin, NOTE_F4, 1000*noteDuration);
+    delay(noteDuration * 1.30 * 1000);
+  }
+  else if(note=='G'){
+    tone(speakerPin, NOTE_G4, 1000*noteDuration);
+    delay(noteDuration * 1.30 * 1000);
+  }
+   else if(note=='Z'){
+    tone(speakerPin, 0, 1000*noteDuration);
+    delay(noteDuration * 1.30 * 1000);
+  }
+  return;
 }
