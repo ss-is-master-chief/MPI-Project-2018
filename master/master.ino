04b7947983cd996b9ebe47701d37e42193acb62f
Anonymous <Anonymous3105@users.noreply.github.com>
2018-03-31 16:47:04 +0530

    Add files via upload
    Serial communication in full swing. Uses Serial.parseFloat() for score Transmission. Score calculation by salve a problem

--- master/master.ino
+++ master/master.ino
@@ -1,7 +1,9 @@
 #include "pitches.h"
+#include <SoftwareSerial.h>
+SoftwareSerial s2(9,10); 
 
 // Setting duration for each note, in seconds
-float noteDuration = 1;
+float noteDuration = 0.5;
 
 // Setting note value, for pitches
 float noteValue = 1/noteDuration;
@@ -19,40 +21,62 @@ int speakerPin = 8;
 long score1, score2;
 int currentNote;
 String noteSequence;
+unsigned long temp = 0;
 
 void setup() {
   //  Opening channel at 9600 Baud
   Serial.begin(9600);
-  
+  s2.begin(9600);
   currentNote = 0;
   noteSequence = "ABCDEZABCDEZABCDEZ";
 
   // Setting maximum value of score1 and score 2, in case one fails to transmit
   score1 = score2 = noteSequence.length()*2.3*noteDuration;
+  delay(1000);
+  Serial.flush();
+  s2.flush();
 }
+
 
 void loop() {
   Serial.flush();
+  s2.flush();
   if(noteSequence[currentNote] == '\0')
   { 
     // Sending STOP signal
     Serial.write('S');
+    s2.write('S');
+    delay(1000);
+
     
     // Getting scores from slaves
     Serial.write('P');
-    if(Serial.available())
-      score1 = Serial.parseFloat();
-    
-    Serial.write('Q');
-    if(Serial.available())
-      score2 = Serial.parseFloat();
+    while(!Serial.available());
+    score1 = Serial.parseFloat();
+    Serial.println(score1);
+//    delay(2000);
+      
+    s2.write('P');
+    while(!s2.available());
+    score2 = s2.parseFloat();
+    Serial.println(score2);
+//    delay(2000);
 
-    if(score1 < score2)
+    if(score1 < score2){
       Serial.write('1');
-    else if (score2 < score1)
+      s2.write('2');
+    }
+    else if (score2 < score1){
       Serial.write('2');
-    else
+      s2.write('1');
+    }
+    else{
       Serial.write('T');
+      s2.write('T');
+    }
+
+    Serial.flush();
+    s2.flush();
 
      delay(resultDelay*1000);
      
@@ -67,32 +91,49 @@ void loop() {
    {
     case 'A':
       Serial.write('A');
+      s2.write('A');
+//      Serial.println('A');
       sing('A', noteDuration);
       break;
     case 'B':
       Serial.write('B');
+      s2.write('B');
+//      Serial.println('B');
       sing('B', noteDuration);
       break;
     case 'C':
       Serial.write('C');
+      s2.write('C');
+//      Serial.println('C');
       sing('C', noteDuration);
       break;    
     case 'D':
       Serial.write('D');
+      s2.write('D');
+//      Serial.println('D');
       sing('D', noteDuration);
       break;
     case 'E':
       Serial.write('E');
+      s2.write('E');
+//      Serial.println('E');      
       sing('E', noteDuration);
       break;
     case 'F':
       Serial.write('F');
+      s2.write('F');
+//      Serial.println('F');
       sing('F', noteDuration);
       break;
     case 'G':
       Serial.write('G');
+      s2.write('G');
+//      Serial.println('G');
       sing('G', noteDuration);
-      break;      
+      break;
+    default:
+      sing('Z', noteDuration);
+      break;
    }
   }
 }
